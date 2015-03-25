bae9ffd9e49c08fed071aea9de8eab0046b94033
Angelique S Kepshire <angelique@watersavvy.info>
2015-03-25 14:14:11 -0500

    updated gem file
    Turbolinks and windows were not playing well together and this seemed to
    fix it.
    
    http://stackoverflow.com/questions/28312460/object-doesnt-support-this-property-or-method-rails-windows-64bit

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'coffee-script-source', '1.8.0'
+#added to fix turbolinks/windows error
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// = require jquery
-// = require bootstrap-sprockets
-= require font-awesome-sprockets
-// = require jquery_ujs
-// = require turbolinks
-// = require_tree .
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
