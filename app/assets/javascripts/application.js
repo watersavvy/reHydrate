0ad0da0a37e632b51633c048bf14c342f24236f7
Angelique S Kepshire <angelique@watersavvy.info>
2015-03-25 10:57:11 -0500

    home view template
    update to copy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 // = require jquery
 // = require bootstrap-sprockets
-// = require font-awesome-sprockets
+= require font-awesome-sprockets
 // = require jquery_ujs
 // = require turbolinks
 // = require_tree .
